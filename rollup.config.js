06b5a85b0ade60213ab7b71e11b944dd2c015fb2
Erick Devv <111036821+ErickDevv@users.noreply.github.com>
2024-02-24 00:46:21 -0600

    first commit

---
+++ rollup.config.js
@@ -0,0 +1,25 @@
+// Contents of the file /rollup.config.js
+import typescript from "@rollup/plugin-typescript";
+import dts from "rollup-plugin-dts";
+
+const config = [
+  {
+    input: "build/compiled/main.js",
+    output: {
+      file: "lib/main.js",
+      format: "es",
+      sourcemap: true,
+    },
+    plugins: [typescript()],
+  },
+  {
+    input: "build/compiled/main.d.ts",
+    output: {
+      file: "lib/main.d.ts",
+      format: "es",
+    },
+    plugins: [dts()],
+  },
+];
+
+export default config;
